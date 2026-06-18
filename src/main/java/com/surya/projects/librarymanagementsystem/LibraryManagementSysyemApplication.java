3d604ec07a4de7e58ba71ee6f014775ab3ee88e8
Surya.V <mrstudieshelper@gmail.com>
2026-06-18 15:06:01 +0530

    initial commit

---
+++ src/main/java/com/surya/projects/librarymanagementsystem/LibraryManagementSysyemApplication.java
@@ -0,0 +1,13 @@
+package com.surya.projects.librarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSysyemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSysyemApplication.class, args);
+	}
+
+}
