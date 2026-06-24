a2f57235dc418ddbdffedaaa5db57cbefa1a3b13
Surya.V <mrstudieshelper@gmail.com>
2026-06-24 20:49:36 +0530

    Add Spring Security Configuration

---
+++ src/main/java/com/surya/configurations/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.surya.configurations;
+
+public class JwtConstant {
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "nofnnanjnaojnsnoasnonasdnndaskandsk";
+
+
+}
---
+++ src/main/java/com/surya/configurations/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.surya.configurations;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+        .sessionManagement(management -> management.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+        ))    
+        .authorizeHttpRequests(Authorize -> Authorize
+            .requestMatchers("/api/**").authenticated()
+            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+            .anyRequest().permitAll()
+        )
+        .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors( cors -> cors.configurationSource(corsConfigurationSource()))
+        .build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedOrigins(
+                    Arrays.asList(
+                        "http://localhost:",
+                        "https://mrstudieshelperlibrary.com"
+                    )
+                );
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Collections.singletonList("Authorization"));
+                cfg.setMaxAge(360L);
+                return cfg;
+            }
+        };
+    }
+
+
+
+}
