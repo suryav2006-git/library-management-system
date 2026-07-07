fa5ebcb9593b5bf878d0fe0b7e59031659228b8c
Surya.V <mrstudieshelper@gmail.com>
2026-07-07 20:57:01 +0530

    Add Razorpay Service

--- Backend/src/main/java/com/surya/services/gateway/RazorpayService.java
+++ Backend/src/main/java/com/surya/services/gateway/RazorpayService.java
@@ -1,36 +1,96 @@
 package com.surya.services.gateway;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.razorpay.PaymentLink;
 import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import com.surya.domain.PaymentType;
 import com.surya.modal.Payment;
 import com.surya.modal.User;
 import com.surya.payload.response.PaymentLinkResponse;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 
 @Service
 @RequiredArgsConstructor
 public class RazorpayService {
 
+    @Value("${razorpay.key.id:}")
+    private String razorpayKeyId;
+
+    @Value("${razorpay.key.secret:}")
+    private String razorpayKeySecret;
+
+    @Value("${razorpay.callback.base-url:http://localhost:5173}")
+    private String callbackBaseUrl;
+
     public PaymentLinkResponse createPaymentLink(User user, Payment payment) {
 
-        @Value("${razorpay.key.id:}")
-        private String razorpayKeyId;
-
-        @Value("${razorpay.key.secret:}")
-        private String razorpayKeySecret;
-
-
         try {
-            RazorpayClient razorpayClient = new RazorpayClient(null, null)
-        } catch {
-            return null ;
+            RazorpayClient razorpayClient = new RazorpayClient(razorpayKeyId, razorpayKeySecret);
+
+            Long amountInPaise = payment.getAmount() * (new java.math.BigDecimal(100)).intValue();
+
+            JSONObject paymentLinkRequest = new JSONObject();
+            paymentLinkRequest.put("amount", amountInPaise);
+            paymentLinkRequest.put("currency", "INR");
+            paymentLinkRequest.put("description", payment.getDescription());
+
+            JSONObject customer = new JSONObject();
+            customer.put("name", user.getFullName());
+            customer.put("email", user.getEmail());
+
+            if (user.getPhone() != null) {
+                customer.put("contact", user.getPhone());
+            }
+
+            paymentLinkRequest.put("customer", customer);
+
+            JSONObject notify = new JSONObject();
+            notify.put("email", user.getEmail());
+            notify.put("sms", user.getPhone() != null);
+            paymentLinkRequest.put("notify", notify);
+
+            paymentLinkRequest.put("remainder_enable", true);
+
+            String successUrl = callbackBaseUrl + "/payment-success" + payment.getId();
+
+            paymentLinkRequest.put("callback_url", successUrl);
+            paymentLinkRequest.put("callback_method", "get");
+
+            JSONObject notes = new JSONObject();
+            notes.put("user_id", user.getId());
+            notes.put("payment_id", payment.getId());
+
+            if (payment.getPaymentType() == PaymentType.MEMBERSHIP) {
+                notes.put("subscription_id", payment.getSubscription().getId());
+                notes.put("plan", payment.getSubscription().getPlan().getPlanCode());
+                notes.put("type", PaymentType.MEMBERSHIP);
+            } else if (payment.getPaymentType() == PaymentType.FINE) {
+                // todo
+                // notes.put("fine_id",payment.getFine().getId());
+                notes.put("type", PaymentType.FINE);
+            }
+            paymentLinkRequest.put("notes", notes);
+
+            PaymentLink paymentLink = razorpayClient.paymentLink.create(paymentLinkRequest);
+
+            String paymentUrl = paymentLink.get("short_url");
+            String paymentLinkId = paymentLink.get("id");
+
+            PaymentLinkResponse response = new PaymentLinkResponse();
+            response.setPayment_link_url(paymentUrl);
+            response.setPayment_link_id(paymentLinkId);
+
+            return response;
+
+        } catch (RazorpayException e) {
+            throw new RuntimeException(e);
         }
 
-
-        return null;
     }
 
 }
