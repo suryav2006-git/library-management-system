4fcb1ee99b2b4bf50bde8590ba4b225348e0d085
Surya.V <mrstudieshelper@gmail.com>
2026-07-26 22:56:23 +0530

    Add Fine Service Impl for Fine Service

--- Backend/src/main/java/com/surya/services/FineService.java
+++ Backend/src/main/java/com/surya/services/FineService.java
@@ -1,5 +1,32 @@
 package com.surya.services;
 
+import java.util.List;
+
+import com.surya.domain.FineStatus;
+import com.surya.domain.FineType;
+import com.surya.payload.dto.FineDTO;
+import com.surya.payload.request.CreateFineRequest;
+import com.surya.payload.request.WaiveFineRequest;
+import com.surya.payload.response.PageResponse;
+import com.surya.payload.response.PaymentInitiateResponse;
+
 public interface FineService {
+
+    FineDTO createFine(CreateFineRequest createFineRequest);
+
+    PaymentInitiateResponse payFine(Long fineId, String transactionId);
+
+    void markFineAsPaid(Long fineId, Long amount, String transactionId);
+
+    FineDTO waiveFine(WaiveFineRequest waiveFineRequest);
+
+    List<FineDTO> getMyFines(FineStatus status, FineType type);
+
+    PageResponse<FineDTO> getAllFines(
+            FineStatus status,
+            FineType type,
+            Long userId,
+            int page,
+            int size);
 
 }
