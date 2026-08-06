c258c4e407ef3213820006d7ce243ae5a82d92db
Surya.V <mrstudieshelper@gmail.com>
2026-08-06 17:55:25 +0530

    Controller for Wishlist

---
+++ Backend/src/main/java/com/surya/controller/WishlistController.java
@@ -0,0 +1,49 @@
+package com.surya.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.surya.payload.dto.WishlistDTO;
+import com.surya.payload.response.ApiResponse;
+import com.surya.payload.response.PageResponse;
+import com.surya.services.WishlistService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+
+@RequestMapping("/api/wishlist")
+public class WishlistController {
+
+    private final WishlistService wishlistService;
+
+    @PostMapping("/add/{bookId}")
+    public ResponseEntity<?> addToWishList(
+            @PathVariable Long bookId,
+            @RequestParam(required = false) String notes) throws Exception {
+        WishlistDTO wishlistDTO = wishlistService.addToWishlist(bookId, notes);
+        return ResponseEntity.ok(wishlistDTO);
+    }
+
+    @DeleteMapping("/remove/{bookId}")
+    public ResponseEntity<ApiResponse> removeFromWishlist(
+            @PathVariable Long bookId) throws Exception {
+        wishlistService.removeFromWishlist(bookId);
+        return ResponseEntity.ok(new ApiResponse(
+                "Book Removed From Wishlist Successfully", true));
+    }
+
+    public ResponseEntity<?> getMyWishlist(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) throws Exception {
+        PageResponse<WishlistDTO> wishlist = wishlistService.getMyWishList(page, size);
+        return ResponseEntity.ok(wishlist);
+    }
+
+}
